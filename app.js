da79b607d78c62b9a18907c86a079c7a79251246
ntt2k1 <nguyentrungthoi7601@gmail.com>
2023-02-24 15:31:08 +0700

    Init code

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const express = require('express');
+const body = require('body-parser');
+const compression = require('compression');
+const path = require('path');
+const port = process.env.port || 1337;
+const app = express();
+const clientFolder = path.join(__dirname, '/UnityRelease');
+ const  { json, urlencoded } = body
+
+app.use(json());
+app.use(compression());
+app.use(urlencoded({ extended: true }));
+app.use(express.static(clientFolder));
+app.use(defaultRoute);
+ 
+http
+    .createServer(app)
+    .listen(port);
+ 
+function defaultRoute(req, response) {
+    response.sendfile(clientFolder + '/index.html');
+}
